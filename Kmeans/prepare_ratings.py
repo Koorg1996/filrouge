b1f9e2050c1030876352e98076093cbd2571fc55
WillBaye <williambaye22@gmail.com>
2020-10-13 17:34:50 +0200

    commit

--- Kmeans/prepare_ratings.py
+++ Kmeans/prepare_ratings.py
@@ -16,6 +16,12 @@ file_user_votecounts = data.groupby(["userId"])["rating"].apply(lambda x : len(l
 file_user_votecounts 
 
 
+fig = px.scatter(x = pd.Series(range(0,len(file_user_votecounts['userId']))), y = file_user_votecounts['voteCount'].sort_values()
+                 , labels=dict( x="users", y="User_votecount") ,title = "Repartition du nombre total de vues par utilisateur" )
+
+
+fig.show()
+
 output_dir = "data_csv/more/"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
@@ -33,7 +39,7 @@ if not os.path.exists(output_dir):
 file_movie_votecounts.to_csv(str(output_dir+"votecounts_movies.csv"))
 str(output_dir+"votecounts.csv")
 
-
+#nombre total de vues par film
 fig = px.scatter(x = pd.Series(range(0,len(file_movie_votecounts['movieId']))), y = file_movie_votecounts['vote_count'].sort_values()
                  , labels=dict( x="movie", y="Movie_votecount") ,title = "Repartition du nombre total de vues par film" )
 
