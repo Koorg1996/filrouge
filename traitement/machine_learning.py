d9895df9ffebe192b491cbcf66cf04c6cbb9d5e4
Julien <julien.mesrine@free.fr>
2020-10-13 19:14:10 +0200

    another one

--- traitement/machine_learning.py
+++ traitement/machine_learning.py
@@ -11,6 +11,10 @@ path = "/home/fitec/donnees_films/"
 final_data_movie = pd.read_csv(path + "final_data_movie.csv")
 ratings = pd.read_csv(path + "clean_ratings.csv")
 
+all_movies = list(final_data_movie.drop_duplicates("id")["id"])
+ratings = ratings[ratings["movieId"].isin(all_movies)]
+
+
 
 
 nbr_votes_user = ratings.groupby("userId")["userId"].count().reset_index(name= "count_user")
@@ -24,6 +28,9 @@ ratings = ratings[ratings["count_movie"]>1000]
 legit_movies = list(ratings.drop_duplicates("movieId")["movieId"])
 final_data_movie = final_data_movie[final_data_movie["id"].isin(legit_movies)]
 
+del nbr_votes_user
+del nbr_votes_movie
+del legit_movies
 
 #Supprimer vote average et vote count
 
@@ -48,20 +55,22 @@ data = data.drop(["vote_count", "vote_average"], axis = 1)
 
 
 #0 ACP sur final_data_movie
-pca = PCA(n_components=110,svd_solver="arpack", random_state=80)
-pca_trans = pd.DataFrame(pca.fit_transform(data))
+#pca = PCA(n_components=110,svd_solver="arpack", random_state=80)
+#pca_trans = pd.DataFrame(pca.fit_transform(data))
+#
+#def somme_ajoutees(liste):
+#    retour = []
+#    calcul = 0
+#    for i in liste:
+#        calcul += i
+#        retour.append(calcul)
+#    return retour
+#
+#explained_variances = somme_ajoutees(pca.explained_variance_ratio_)
+#
+#plt.scatter(x = [i-1 for i in range(1,len(explained_variances) + 1)],y = explained_variances)
 
-def somme_ajoutees(liste):
-    retour = []
-    calcul = 0
-    for i in liste:
-        calcul += i
-        retour.append(calcul)
-    return retour
 
-explained_variances = somme_ajoutees(pca.explained_variance_ratio_)
-
-plt.scatter(x = [i-1 for i in range(1,len(explained_variances) + 1)],y = explained_variances)
 
 #Il semblerait par la méthode du coude que les 25 premieres composantes sont à sélectionner pour résumer la donnée
 
@@ -83,14 +92,14 @@ data = pd.DataFrame(pca.transform(data))
 
 #1 Realisation du kmeans sur final_data_movie
 #méthode du coude pour déterminer le nombre de clusters
-inerties = []
-for i in range(1,15):
-    kmeans = KMeans(n_clusters=i, random_state=0).fit(data)
-    inerties.append(kmeans.inertia_)
+#inerties = []
+#for i in range(1,15):
+#    kmeans = KMeans(n_clusters=i, random_state=0).fit(data)
+#    inerties.append(kmeans.inertia_)
+#
+#plt.plot(inerties)
 
-plt.plot(inerties)
-
-#on garde k=4
+#on garde k=8
 
 k = 8
 kmeans = KMeans(n_clusters=k, random_state=0, n_init=10).fit(data)
@@ -98,16 +107,16 @@ final_data_movie["cluster_movie"] = kmeans.labels_
 kmeans.n_iter_
 
 # Les 10 meilleurs films de chaque cluster movie
-print(final_data_movie.groupby("cluster_movie")["cluster_movie"].count())
-movies = []
-clusters = []
-for i in range(k):
-    cluster = final_data_movie[final_data_movie["cluster_movie"] == i]
-    best_movies = cluster.sort_values("vote_average", ascending = False).head(30)
-    movies.append(list(best_movies["title"]))
-    clusters.append(i)
-d = {'clusters':clusters,'movies':movies}
-best_movies_cluster_movie = pd.DataFrame(d)
+#print(final_data_movie.groupby("cluster_movie")["cluster_movie"].count())
+#movies = []
+#clusters = []
+#for i in range(k):
+#    cluster = final_data_movie[final_data_movie["cluster_movie"] == i]
+#    best_movies = cluster.sort_values("vote_average", ascending = False).head(30)
+#    movies.append(list(best_movies["title"]))
+#    clusters.append(i)
+#d = {'clusters':clusters,'movies':movies}
+#best_movies_cluster_movie = pd.DataFrame(d)
 description_clustering = final_data_movie.groupby(["cluster_movie"]).sum()
 
 
@@ -154,9 +163,18 @@ final_data_user = final_data_user.fillna(3.5)
 
 #2 kmeans utilisateurs
 
+#On calcule la moyenne des note spar utilisateur afin de ne pas se retrouver avec un clustering de type :
+# groupe des utilisteurs qui notent bien, groupe des utilisateurs qui notent mal ,ect
 
+moy_by_user = ratings.groupby("userId")["rating"].mean().reset_index(name="note_moy_user")
+final_data_user = pd.merge(final_data_user, moy_by_user, left_on = "userId", right_on="userId")
 
 data = final_data_user.drop(['userId', 'compte_0', 'compte_1','compte_2', 'compte_3','compte_4', 'compte_5','compte_6', 'compte_7' ], axis=1)
+
+for col in data.drop("note_moy_user", axis=1).columns:
+    data[col] = data[col]/data["note_moy_user"]
+
+data = data.drop("note_moy_user", axis=1)
 
 #méthode du coude pour déterminer le nombre de clusters
 inerties = []
@@ -180,7 +198,7 @@ ratings_cluster = pd.merge(ratings_cluster, join, left_on="userId", right_on='us
 
 
 
-# 
+# Les films les plus aimés pour chaque cluster d'utilisateur
 a = ratings_cluster.groupby(["cluster_user","movieId"])["rating"].count().reset_index(name="count")
 b = ratings_cluster.groupby(["cluster_user","movieId"])["rating"].mean().reset_index(name="mean")
 a["mean"] = b["mean"]
@@ -191,7 +209,7 @@ best_movies_per_cluster = pd.merge(best_movies_per_cluster, final_data_movie, le
 
 w = best_movies_per_cluster.groupby(["cluster_user", "cluster_movie"])["mean"].count().reset_index(name="count_per_cluster")
 
-
+# A faire : recommander 3 films à chaque utilisateur à partir des films les plus aimés de son cluster et qu'il n'a pas vu
 
 
 
